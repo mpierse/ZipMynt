614b1ea6b31f9b926f58856e685c3c53a736280b
mpierse <mpierse@udel.edu>
2018-12-20 15:07:14 -0500

    remove all

--- src/main/java/io/github/jhipster/application/domain/enumeration/Category.java
+++
@@ -1,8 +0,0 @@
-package io.github.jhipster.application.domain.enumeration;
-
-/**
- * The Category enumeration.
- */
-public enum Category {
-    RENT, FOOD, INCOME, UTILITIES, SHOPPING, TRANSFER, AUTOMOTIVE, MISCELLANEOUS
-}
--- src/main/java/io/github/jhipster/application/domain/enumeration/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package io.github.jhipster.application.domain.enumeration;
-
-/**
- * The TransactionType enumeration.
- */
-public enum TransactionType {
-    CREDIT, DEBIT
-}
