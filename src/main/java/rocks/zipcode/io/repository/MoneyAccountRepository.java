7b8716c03aa60e82b6b01803a6aee9afd2b39dda
mpierse <mpierse@udel.edu>
2019-01-01 18:12:10 -0500

    csvs updated and query added with join

--- src/main/java/rocks/zipcode/io/repository/MoneyAccountRepository.java
+++ src/main/java/rocks/zipcode/io/repository/MoneyAccountRepository.java
@@ -1,6 +1,9 @@
 package rocks.zipcode.io.repository;
 
 import rocks.zipcode.io.domain.MoneyAccount;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MoneyAccountRepository extends JpaRepository<MoneyAccount, Long> {
 
+    @Query(value = "SELECT * FROM MONEY_ACCOUNT", nativeQuery = true)
+    List<MoneyAccount> findByUserIsCurrentUser();
 }
