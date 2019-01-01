ca7e714dc584e0fa59f44f8ddc60e50161e35913
Trinh Tong <trt.tong@gmail.com>
2019-01-01 16:44:34 -0500

    testing changes

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,17 @@ import { RouterModule } from '@angular/router';
 import { ZipmyntSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
 import { NgxPlaidLinkModule } from 'ngx-plaid-link';
+import { ZipmyntMoneyAccountModule } from '../entities/money-account/money-account.module';
+import { ZipmyntTransactionModule } from '../entities/transaction/transaction.module';
 
 @NgModule({
-    imports: [ZipmyntSharedModule, NgxPlaidLinkModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [
+        ZipmyntSharedModule,
+        NgxPlaidLinkModule,
+        ZipmyntTransactionModule,
+        ZipmyntMoneyAccountModule,
+        RouterModule.forChild([HOME_ROUTE])
+    ],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
